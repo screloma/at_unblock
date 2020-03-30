ba105ab31f156cced6d315e03b510698f698b1ce
screloma <screloma@gmail.com>
2020-03-31 00:19:19 +0300

    Create main.user.js

---
+++ main.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Author Today unblocker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://author.today/reader/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let style = document.createElement('style');
+    style.innerHTML = '*{ user-select: auto !important; webkit-user-select: auto !important; }';
+    $('.noselect')[0].onmousedown = e=>true;
+    $('.noselect')[0].onselectstart = e=>true;
+    $('.noselect')[0].oncontextmenu = e=>true
+    document.body.appendChild(style);
+})();
